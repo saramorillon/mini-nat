7e85fabcd654f40f6a8e6816bfe07a3624c7939d
Sara Morillon <sara.morillon.pro@gmail.com>
2021-09-25 19:14:42 +0200

    Add some logs

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,13 @@ const configs = readConfigs()
 
 for (const config of configs) {
   const socket = net.createServer((instream) => {
+    console.log(`Stream connected from ${config.out.host}:${config.out.port} to ${config.in.host}:${config.in.port}`)
     const outstream = net.createConnection(config.out)
     instream.pipe(outstream)
     outstream.pipe(instream)
   })
 
-  socket.listen(config.in.port, config.in.host)
+  socket.listen(config.in.port, config.in.host, () => {
+    console.log(`Listening on ${config.in.host}:${config.in.port}`)
+  })
 }
